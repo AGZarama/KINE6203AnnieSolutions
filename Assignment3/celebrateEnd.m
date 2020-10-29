daf7fe1112a578a13b24738d0cd3d1a552aae680
AGZarama <72218982+AGZarama@users.noreply.github.com>
2020-10-28 22:02:41 -0400

    Create celebrateEnd.m

---
+++ Assignment3/celebrateEnd.m
@@ -0,0 +1,7 @@
+function [message] = celebrateEnd ()
+% This function takes no input. When called, it displays as output a
+% cellebratory message.
+
+message = "Thank goodness this assignment is over" ;
+
+end
