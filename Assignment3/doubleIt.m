f5a0e2268506653b71518ac020f4ffb569dda5e4
AGZarama <72218982+AGZarama@users.noreply.github.com>
2020-10-28 22:02:47 -0400

    Create doubleIt.m

---
+++ Assignment3/doubleIt.m
@@ -0,0 +1,6 @@
+function [numberDouble] = doubleIt(numberOriginal)
+%This function takes one integer as input and then returns a value that is
+%double the original value.
+
+numberDouble = (numberOriginal) *2;
+end
